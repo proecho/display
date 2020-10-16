d264cb30a5e9b393fb5d342e536110f72cdca696
proecho <niamhgillespie2016@outlook.com>
2020-10-16 18:03:59 +0100

    just started

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::LinkedList;
+use objects;
+
+fn load_days_reminders()->LinkedList<entrys>{
+	Date = DateTime::<Utc>::from(SystemTime::now()).date()
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
